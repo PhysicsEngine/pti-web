aa94e3f128553f434b8c82017500ddb1a246a5dd
tishibas <t_ishibas67@yahoo.co.jp>
2017-02-05 10:04:48 +0900

    add pagination

--- articles/views.py
+++ articles/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Authors
 from .models import Articles
 
-
-def index(request):
-    authors = Authors.objects.order_by('-rate')
-    context = {
-        'authors': authors
-    }
-    return render(request, 'articles/index.html', context)
 
 def author(request, author_id):
     author = Authors.objects.filter(id=author_id)[0]
@@ -21,5 +13,22 @@ def author(request, author_id):
         'articles': articles,
     }
     return render(request, 'articles/author.html', context)
+
+
+def index(request):
+    authors = Authors.objects.order_by('-rate')
+    paginator = Paginator(authors, 25)
+    page = request.GET.get('page')
+
+    try:
+        extacted_authors = paginator.page(page)
+    except PageNotAnInteger:
+        extacted_authors = paginator.page(1)
+    except EmptyPage:
+        extacted_authors = paginator.page(paginator.num_pages)
+    context = {
+        'authors': extacted_authors,
+    }
+    return render(request, 'articles/author_list.html', context)
 
 
